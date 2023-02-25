5d6707a501e9836539ac8f91e5a4e6f43799e45c
Rafael S. de Lima <rafael.lima@startse.com>
2023-02-25 16:30:57 -0300

    adicionar a mensagem que o usuário foi cadastrado com sucesso

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let inputNomeCompleto = document.getElementById('nome_completo')
 let inputEmail = document.getElementById('email')
 let spanNomeCompleto = document.getElementById('span-nome-completo')
 let spanEmail = document.getElementById('span-email')
+let spanSuccess = document.getElementById('span-success')
 function onSubmit(event) {
     event.preventDefault();
     if (inputNomeCompleto.value === '') {
@@ -15,4 +16,14 @@ function onSubmit(event) {
     } else {
         spanEmail.style.display = 'none'
     }
+
+    if(inputNomeCompleto.value !== '' && inputEmail.value !== '') {
+        spanSuccess.style.display = 'block'
+        inputNomeCompleto.value = ''
+        inputEmail.value = ''
+    } else {
+        spanSuccess.style.display = 'none'
+    }
+
+    
 }
